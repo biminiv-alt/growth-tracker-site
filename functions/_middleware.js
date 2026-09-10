d1effa7f7d7c72651fc55d8b3d879f98e8de88fb
biminiv-alt <biminiv@gmail.com>
2026-09-10 14:48:05 -0400

    Middleware: palette swap fallback + analytics beacon

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,14 +1,27 @@
-export const onRequest = async ({ request, next }) => {
+const SWAP = {
+  '#f4f7f2': '#f0ede4','#e6ede1': '#e5e0d2','#b8c9ac': '#a5b0a0',
+  '#7d9b73': '#4a5d55','#4f6a4a': '#2d3d34','#2b3a29': '#1a2e26',
+  '#e8b39a': '#d9c5a5','#cf7955': '#b8935a','#a15837': '#8a6b3d',
+  '#fbf7f1': '#f7f3ec','#2a2723': '#1a2e26','#5a5651': '#4a5d55',
+  '#e5ded2': '#d9d0c1'
+};
+const BEACON = '<script defer src="https://static.cloudflareinsights.com/beacon.min.js" data-cf-beacon=\'{"token":"8169c71f4b174d1a88ea5a5732671a6b"}\'></script>';
+export const onRequest = async ({ next }) => {
   const response = await next();
-  const contentType = response.headers.get('content-type') || '';
-  if (!contentType.includes('text/html')) return response;
-  const rewriter = new HTMLRewriter().on('body', {
-    element(el) {
-      el.append(
-        '<script defer src="https://static.cloudflareinsights.com/beacon.min.js" data-cf-beacon=\'{"token":"8169c71f4b174d1a88ea5a5732671a6b"}\'></script>',
-        { html: true }
-      );
-    }
-  });
-  return rewriter.transform(response);
+  const ct = response.headers.get('content-type') || '';
+  if (!ct.includes('text/html')) return response;
+  let html = await response.text();
+  for (const [o, n] of Object.entries(SWAP)) {
+    html = html.split(o).join(n);
+    const O = o.toUpperCase();
+    if (O !== o) html = html.split(O).join(n);
+  }
+  if (/<\/body>/i.test(html)) {
+    html = html.replace(/<\/body>/i, BEACON + '</body>');
+  } else {
+    html += BEACON;
+  }
+  const headers = new Headers(response.headers);
+  headers.delete('content-length');
+  return new Response(html, { status: response.status, statusText: response.statusText, headers });
 };
